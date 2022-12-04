03bb06c8798a8fed90d411a1da731a687e68b040
der-meister03 <acmilanp7@hotmail.com>
2022-12-04 17:13:40 -0600

    git credential manager added

--- git/git_config.sh
+++ git/git_config.sh
@@ -8,9 +8,12 @@ echo "please provide your e-mail";
 read email_git;
 echo "please provide your git's user name";
 read user_git;
+echo "please provide git credential manager package"
+read gcm_package
 
 git config --global user.email $email_git;
 git config --global user.name  $user_git;
+git config --global credential.credentialStore gpg
 
 repo_dir="/home/$USER/Repositories";
 
@@ -18,6 +21,10 @@ mkdir $repo_dir;
 chown -R $USER:$USER $repo_dir;
 
 git config --global alias.lg "log --pretty='%Cred%h%Creset |%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset %C(cyan)[%an]%Creset' --graph"
+
+sudo dpkg -i $gcm_package
+git-credential-manager configure
+
 
 echo "###################################################";
 echo "################## Git configured #################";
