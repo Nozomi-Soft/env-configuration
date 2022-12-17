df277774f349a29476074bc88d8cafad4a86cb33
der-meister03 <acmilanp7@hotmail.com>
2022-12-16 23:53:54 -0600

    Network name variable added

--- docker/mysql_image.sh
+++ docker/mysql_image.sh
@@ -1,26 +1,38 @@
 sudo docker pull mysql/mysql-server:latest;
 
-sudo docker volume create mysqlvolume;
+echo "Enter a container name (mysql-dev)";
+read  CONTAINER_NAME;
+
+echo "Enter a volume name (mysqlvolume)";
+read  VOLUME_NAME;
+
+echo "Enter a port number (9501)";
+read  PORT_MYSQL;
+
+sudo docker volume create $VOLUME_NAME;
 
 #sudo docker run --name=mysql-dev -d mysql/mysql-server:latest -p 3306:3306;
 
 sudo docker run -d -e MYSQL_ROOT_PASSWORD=123456 \
--p 9501:3306 \
---name mysql-dev \
---mount source=mysqlvolume,target=/app \
+-p $PORT_MYSQL:3306 \
+--name $CONTAINER_NAME \
+--mount source=$VOLUME_NAME,target=/app \
 mysql/mysql-server:latest;
 
-sudo docker logs mysql-dev;
+sudo docker logs $CONTAINER_NAME;
 
-sudo docker network connect nozomi-net mysql-dev
+echo "Enter a network name";
+read NETWORK_NAME;
+
+sudo docker network connect $NETWORK_NAME $CONTAINER_NAME
 
 #sudo docker exec -it mysql-dev mysql -u root -p
 
-#ALTER USER 'root'@'localhost' IDENTIFIED BY 'newpassword';
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '<new_password>';
 
-#CREATE USER 'nozomi'@'%' IDENTIFIED BY 'pass';
-#GRANT ALL PRIVILEGES ON *.* TO 'nozomi'@'%' WITH GRANT OPTION;
+#CREATE USER '<user_name>'@'%' IDENTIFIED BY 'pass';
+#GRANT ALL PRIVILEGES ON *.* TO '<user_name>'@'%' WITH GRANT OPTION;
 #FLUSH PRIVILEGES;
 
-#CREATE DATABASE nozomidb;
-#USE nozomidb;
+#CREATE DATABASE <database_name>;
+#USE <database_name>;
