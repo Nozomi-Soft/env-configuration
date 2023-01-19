4f57141fd0e49bca5b2d51636e2ee92c38fce2b7
der-meister03 <acmilanp7@hotmail.com>
2023-01-18 20:05:11 -0600

    New format style

--- init_config.sh
+++ init_config.sh
@@ -1,82 +1,99 @@
-echo "###################################################";
-echo "#		             Debian Configuration     	      #";
-echo "###################################################";
+echo "#####################################################################"
+echo "#		             Debian Configuration     	                      #"
+echo "#####################################################################"
 
-echo "Please provide your linux's user_name";
-read user_name;
+echo "Please provide your linux's user_name"
+read user_name
 
-usermod -aG root $user_name;
-echo "source /etc/enviroment" | tee -a /root/.bashrc;
-source /etc/enviroment;
+usermod -aG root $user_name
+echo "source /etc/enviroment" | tee -a /root/.bashrc
+source /etc/enviroment
 
-echo "###################################################";
-echo "#		             Sources Configuration   		      #";
-echo "###################################################";
+echo "#####################################################################"
+echo "#		             Sources Configuration   		                  #"
+echo "#####################################################################"
 
-debian_ver=`lsb_release -c -s`;
+debian_ver=`lsb_release -c -s`
 
-sed -i '/cdrom/ s/#*//' /etc/apt/sources.list;
-sed -i '/cdrom/ s/^/#/' /etc/apt/sources.list;
-#echo "deb http://deb.debian.org/debian/ $debian_ver main contrib" | tee -a /etc/apt/sources.list;
-#echo "deb-src http://deb.debian.org/debian/ $debian_ver main contrib" | tee -a /etc/apt/sources.list;
-#echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee -a /etc/apt/sources.list;
-#add-apt-repository "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main";
-apt-get -y -f install software-properties-common;
-#wget -q -O - http://dl.google.com/linux/linux_signing_key.pub | apt-key add -;
+sed -i '/cdrom/ s/#*//' /etc/apt/sources.list
+sed -i '/cdrom/ s/^/#/' /etc/apt/sources.list
+#echo "deb http://deb.debian.org/debian/ $debian_ver main contrib" | tee -a /etc/apt/sources.list
+#echo "deb-src http://deb.debian.org/debian/ $debian_ver main contrib" | tee -a /etc/apt/sources.list
+wget -q -O - http://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee -a /etc/apt/sources.list.d/google-chrome.list
+#add-apt-repository "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
 
-echo "===================================================";
-echo "#		             Sources Configuration		        #";
-echo "===================================================";
+echo "===================================================================="
+echo "#		             software-properties-common                      #"
+echo "===================================================================="
 
-echo "###################################################";
-echo "#		                 apt-get update           	  #";
-echo "###################################################";
+apt-get -y -f install software-properties-common
 
-apt update;
-apt-get update;
-apt-get upgrade;
 
-apt-get -y install build-essential;
-echo "===================================================";
-echo "#		               build-essential			          #";
-echo "===================================================";
+echo "####################################################################"
+echo "#		                 apt-get update           	                 #"
+echo "####################################################################"
 
-apt-get -f install net-tools;
-echo "===================================================";
-echo "#		                 net-tools                    #";
-echo "===================================================";
+apt-get update
+apt-get upgrade
 
-apt-get -y install sudo;
-echo "${user_name} ALL=(ALL:ALL) ALL" | tee -a /etc/sudoers;
-sudo usermod -aG sudo $user_name;
-echo "===================================================";
-echo "#		       			       sudo                       #";
-echo "===================================================";
+echo "===================================================================="
+echo "#		               build-essential			                     #"
+echo "===================================================================="
 
-sudo apt-get -f -y install maven;
+apt-get -y install build-essential
 
-echo "===================================================";
-echo "#                     maven                       #";
-echo "===================================================";
+echo "===================================================================="
+echo "#		                 net-tools                                   #"
+echo "===================================================================="
 
-apt-get -y install openssh-server;
+apt-get -f install net-tools
+
+echo "===================================================================="
+echo "#		       			       sudo                                  #"
+echo "===================================================================="
+
+apt-get -y install sudo
+echo "${user_name} ALL=(ALL:ALL) ALL" | tee -a /etc/sudoers
+usermod -aG sudo $user_name
+
+echo "===================================================================="
+echo "#                     maven                                        #"
+echo "===================================================================="
+
+apt-get -f -y install maven
+
+echo "===================================================================="
+echo "#		               openssh-server                                #"
+echo "===================================================================="
+
+apt-get -y install openssh-server
 #/etc/ssh/shhd_config
 #/etc/init.d/ssh restart
-rm /etc/ssh/ssh_host_*;
-dpkg-reconfigure openssh-server;
-sed -i "/Port\ 22/ s/#*//" /etc/ssh/sshd_config;
-echo "===================================================";
-echo "#		               openssh-server                 #";
-echo "===================================================";
+rm /etc/ssh/ssh_host_*
+echo "sshd : ALL" | tee -a /etc/host.allow
+sed -i "/Port\ 22/ s/#*//" /etc/ssh/sshd_config
+sudo dpkg-reconfigure openssh-server
+systemctl restart ssh.service
 
-apt-get -y -f install google-chrome-stable;
-echo "===================================================";
-echo "#     		       google-chrome-stable             #";
-echo "===================================================";
+echo "===================================================================="
+echo "#	               openssh-server   " | echo `systemctl status ssh.service`
+echo "===================================================================="
 
+echo "===================================================================="
+echo "#     		       google-chrome-stable                          #"
+echo "===================================================================="
 
-apt-get install pass
+apt-get -y -f install google-chrome-stable
+
+echo "===================================================================="
+echo "#                 password-storage-key                             #"
+echo "===================================================================="
+
+apt-get -f -y install pass
 pass init "my password storage key"
-echo "===================================================";
-echo "#                 password-storage-key            #";
-echo "===================================================";
+
+
+echo "===================================================================="
+echo "#                           Done                                   #"
+echo "===================================================================="
