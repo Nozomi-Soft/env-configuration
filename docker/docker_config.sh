1f48d25b54dcd1cb74fbd23a6f333ad06630413a
der-meister03 <acmilanp7@hotmail.com>
2023-01-24 14:10:15 -0600

    Moving packages to init_config file

--- docker/docker_config.sh
+++ docker/docker_config.sh
@@ -1,14 +1,5 @@
 debian_ver=`lsb_release -cs`;
 
-sudo apt-get update;
-
-sudo apt-get install apt-transport-https;
-sudo apt-get install ca-certificates;
-sudo apt-get install curl;
-sudo apt-get install gnupg2;
-sudo apt-get install software-properties-common;
-
-
 curl -fsSl https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
 echo `sudo apt-key fingerprint 0EBFCD88`;
