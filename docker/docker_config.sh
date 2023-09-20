5ed2c43aa07640e5d35474845b234881069ad6dd
der-meister03 <acmilanp7@hotmail.com>
2023-09-20 01:11:52 -0500

    MySQL compose file

--- docker/docker_config.sh
+++ docker/docker_config.sh
@@ -2,7 +2,7 @@ debian_ver=`lsb_release -cs`;
 
 sudo install -m 0755 -d /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
--- docker/mysql_image.sh
+++ docker/mysql_image.sh
@@ -1,38 +1,56 @@
-sudo docker pull mysql/mysql-server:latest;
+#sudo docker pull mysql/mysql-server:latest;
 
-echo "Enter a container name (mysql-dev)";
-read  CONTAINER_NAME;
+#echo "Enter a container name (mysql-dev)";
+#read  CONTAINER_NAME;
 
-echo "Enter a volume name (mysqlvolume)";
-read  VOLUME_NAME;
+#echo "Enter a volume name (mysqlvolume)";
+#read  VOLUME_NAME;
 
-echo "Enter a port number (9501)";
-read  PORT_MYSQL;
+#echo "Enter a port number (9501)";
+#read  PORT_MYSQL;
 
-sudo docker volume create $VOLUME_NAME;
+#sudo docker volume create $VOLUME_NAME;
 
 #sudo docker run --name=mysql-dev -d mysql/mysql-server:latest -p 3306:3306;
 
-sudo docker run -d -e MYSQL_ROOT_PASSWORD=123456 \
--p $PORT_MYSQL:3306 \
---name $CONTAINER_NAME \
---mount source=$VOLUME_NAME,target=/app \
-mysql/mysql-server:latest;
+#sudo docker run -d -e MYSQL_ROOT_PASSWORD=123456 \
+#-p $PORT_MYSQL:3306 \
+#--name $CONTAINER_NAME \
+#--mount source=$VOLUME_NAME,target=/app \
+#mysql/mysql-server:latest;
 
-sudo docker logs $CONTAINER_NAME;
+#sudo docker logs $CONTAINER_NAME;
 
-echo "Enter a network name";
-read NETWORK_NAME;
+#source .env
+sudo docker compose -f mysql-compose.yaml up -d
 
-sudo docker network connect $NETWORK_NAME $CONTAINER_NAME
+#echo "Enter a network name";
+#read NETWORK_NAME;
+
+#sudo docker network connect $NETWORK_NAME $CONTAINER_NAME
+
+
+
+
+
+#open the bash and give permissions 
+#sudo docker exec -it docker-db-1 bash
+
+#chmod 777 /var/run/mysqld/mysqld.sock
+#chown -R $USER:$GROUP /var/lib/mysql
+#chown -R $USER:$GROUP /var/log/mysql
+#create the dir for logs
+#mkdir /var/log/mysql
+#mysqladmin -u root -p shutdown
+
+
+
 
 #sudo docker exec -it mysql-dev mysql -u root -p
 
-#ALTER USER 'root'@'localhost' IDENTIFIED BY '<new_password>';
-
-#CREATE USER '<user_name>'@'%' IDENTIFIED BY 'pass';
 #GRANT ALL PRIVILEGES ON *.* TO '<user_name>'@'%' WITH GRANT OPTION;
 #FLUSH PRIVILEGES;
 
-#CREATE DATABASE <database_name>;
+
+
 #USE <database_name>;
